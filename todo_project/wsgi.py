d74f27c35894331983eb61df90d38e85a70751f4
Pranav Kumbhojkar <149917723+PRANAVK3004@users.noreply.github.com>
2024-12-10 10:51:49 +0530

    Update wsgi.py

--- todo_project/wsgi.py
+++ todo_project/wsgi.py
@@ -1,8 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_project.settings')
 
-# Create the WSGI application
 application = get_wsgi_application()
